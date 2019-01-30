50afecdc9f90263b0b9747bbc7c877af3fc5f76c
Paulo <p.henriquerm@hotmail.com>
2019-01-30 17:56:18 -0300

    Adcionando adpter a listView

--- app/src/main/java/com/exemplo/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/NumbersActivity.java
@@ -1,18 +1,12 @@
 package com.exemplo.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -21,25 +15,23 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
 
-        ArrayList<String> words = new ArrayList<>();
+        ArrayList<Word> words = new ArrayList<>();
 
-        words.add("um");
-        words.add("dois");
-        words.add("três");
-        words.add("quatro");
-        words.add("cinco");
-        words.add("seis");
-        words.add("sete");
-        words.add("oito");
-        words.add("nove");
-        words.add("dez");
+        words.add(new Word("um", "lutti"));
+        words.add(new Word("dois", "otiiko"));
+        words.add(new Word("três", "tolookosu"));
+        words.add(new Word("quatro", "oyyisa"));
+        words.add(new Word("cinco", "massokka"));
+        words.add(new Word("seis", "temmokka"));
+        words.add(new Word("sete", "kenekaku"));
+        words.add(new Word("oito", "kawinta"));
+        words.add(new Word("nove", "wo'e"));
+        words.add(new Word("dez", "na'aacha"));
 
+        WordAdpter adpter = new WordAdpter(this, words);
 
-        ArrayAdapter<String> itemsAdpter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
-
-        ListView listView = findViewById(R.id.rootView);
-
-        listView.setAdapter(itemsAdpter);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adpter);
     }
 }
 
