e102c782b4c4598a66b520301167445dc0623e52
Paulo <p.henriquerm@hotmail.com>
2019-01-31 14:25:45 -0300

    Implementando logica da PhrasesActivity

--- app/src/main/java/com/exemplo/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/PhrasesActivity.java
@@ -3,12 +3,33 @@ package com.exemplo.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("Onde você está indo?", "minto wuksus"));
+        words.add(new Word("Qual seu nome?", "tinnә oyaase'nә"));
+        words.add(new Word("Meu nome é...", "oyaaset..."));
+        words.add(new Word("Como você está se sentido?", "michәksәs?"));
+        words.add(new Word("Estou me sentindo bem.", "kuchi achit"));
+        words.add(new Word("Você está vindo?", "әәnәs'aa?"));
+        words.add(new Word("Sim, estou indo.", "hәә’ әәnәm"));
+        words.add(new Word("Estou indo.", "әәnәm"));
+        words.add(new Word("Vamos lá.", "yoowutis"));
+        words.add(new Word("Venha aqui.", "әnni'nem"));
+
+        WordAdpter adpter = new WordAdpter(this, words);
+
+        ListView listView = findViewById(R.id.word_list);
+        listView.setAdapter(adpter);
     }
 }
