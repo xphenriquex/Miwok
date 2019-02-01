3a5829de43c2ae8c60bf1e085dcd6a0cc7df10f2
Paulo <p.henriquerm@hotmail.com>
2019-02-01 16:34:19 -0300

    Adcionado cor no construtor, validando visibilidade de imagens e setando a cor no background

--- app/src/main/java/com/exemplo/android/miwok/WordAdpter.java
+++ app/src/main/java/com/exemplo/android/miwok/WordAdpter.java
@@ -1,10 +1,13 @@
 package com.exemplo.android.miwok;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,9 +16,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class WordAdpter extends ArrayAdapter<Word> {
+    private int mColor;
 
-    public WordAdpter(@NonNull Context context, ArrayList<Word> words) {
+    public WordAdpter(@NonNull Context context, ArrayList<Word> words, int color) {
         super(context, 0, words);
+        mColor = color;
     }
 
     @NonNull
@@ -23,7 +28,7 @@ public class WordAdpter extends ArrayAdapter<Word> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         //Get the data item for this position
-        Word word = getItem(position);
+        Word currentWord = getItem(position);
 
         //Check if an existing view is being reused, otherwise inflate the view
         View listView = convertView;
@@ -35,10 +40,27 @@ public class WordAdpter extends ArrayAdapter<Word> {
         //Look up view for data  population
         TextView miwokTranslation = listView.findViewById(R.id.miwok_text_view);
         TextView defaultTranslation = listView.findViewById(R.id.dafault_text_view);
+        ImageView img = listView.findViewById(R.id.miwok_image_view);
+        View layoutText = listView.findViewById(R.id.layout_text);
 
         // populate the data into the template view using the data object
-        miwokTranslation.setText(word.getMwordMiwokTranslation());
-        defaultTranslation.setText(word.getmWordDefaultTranslation());
+        miwokTranslation.setText(currentWord.getMiwokTranslation());
+
+        defaultTranslation.setText(currentWord.getDefaultTranslation());
+
+        layoutText.setBackgroundResource(mColor);
+
+        if(currentWord.hasImage()){
+            //set the imageView to the image resource specified in the current Word
+            img.setImageResource(currentWord.getImageResourceID());
+
+            //Make sure the view is visible
+            img.setVisibility(View.VISIBLE);
+        }else{
+            //Otherwise hide the ImageView (set visibility to GONE)
+            img.setVisibility(View.GONE);
+        }
+
 
         return listView;
     }
