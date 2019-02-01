3aa6af30530e2d1c43a683af75a1c2f565ff6d17
Paulo <p.henriquerm@hotmail.com>
2019-02-01 16:09:28 -0300

    Adcionando imagens de familiares

--- app/src/main/java/com/exemplo/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/FamilyActivity.java
@@ -16,16 +16,16 @@ public class FamilyActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<>();
 
-        words.add(new Word("pai", "әpә"));
-        words.add(new Word("mãe", "әta"));
-        words.add(new Word("filho", "angsi"));
-        words.add(new Word("filha", "tune"));
-        words.add(new Word("irmão mais velho", "taachi"));
-        words.add(new Word("irmão mais novo", "chalitti"));
-        words.add(new Word("irmã mais velha", "teṭe"));
-        words.add(new Word("irmã mais nova", "kolliti"));
-        words.add(new Word("avó", "ama"));
-        words.add(new Word("avô", "paapa"));
+        words.add(new Word("pai", "әpә", R.drawable.family_father));
+        words.add(new Word("mãe", "әta", R.drawable.family_mother));
+        words.add(new Word("filho", "angsi", R.drawable.family_son));
+        words.add(new Word("filha", "tune", R.drawable.family_daughter));
+        words.add(new Word("irmão mais velho", "taachi", R.drawable.family_older_brother));
+        words.add(new Word("irmão mais novo", "chalitti", R.drawable.family_younger_brother));
+        words.add(new Word("irmã mais velha", "teṭe", R.drawable.family_older_sister));
+        words.add(new Word("irmã mais nova", "kolliti", R.drawable.family_younger_sister));
+        words.add(new Word("avó", "ama", R.drawable.family_grandmother));
+        words.add(new Word("avô", "paapa", R.drawable.family_grandfather));
 
         WordAdpter adpter = new WordAdpter(this, words);
 
