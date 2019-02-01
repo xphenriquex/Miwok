09c721fb47db5db22e69de3ea5a9d1d113ffaeba
Paulo <p.henriquerm@hotmail.com>
2019-02-01 16:14:08 -0300

    Adcionando Image, e metodos para validar

--- app/src/main/java/com/exemplo/android/miwok/Word.java
+++ app/src/main/java/com/exemplo/android/miwok/Word.java
@@ -1,21 +1,37 @@
 package com.exemplo.android.miwok;
 
+
 public class Word {
 
     private String mWordDefaultTranslation;
     private String mwordMiwokTranslation;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation){
+    public Word(String defaultTranslation, String miwokTranslation) {
         mWordDefaultTranslation = defaultTranslation;
         mwordMiwokTranslation = miwokTranslation;
     }
 
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mWordDefaultTranslation = defaultTranslation;
+        mwordMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
 
-    public String getmWordDefaultTranslation() {
+    public String getDefaultTranslation() {
         return mWordDefaultTranslation;
     }
 
-    public String getMwordMiwokTranslation() {
+    public String getMiwokTranslation() {
         return mwordMiwokTranslation;
+    }
+
+    public int getImageResourceID() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
