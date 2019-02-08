ef7304b889b7d052f105301b8ee5debcfa25ca41
Paulo <p.henriquerm@hotmail.com>
2019-02-08 14:03:22 -0300

    Passando audios dos familiares para a lista de palavras

--- app/src/main/java/com/exemplo/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/FamilyActivity.java
@@ -2,7 +2,11 @@ package com.exemplo.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,22 +19,24 @@ public class FamilyActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<>();
+        LinearLayout layoutText = findViewById(R.id.layout_text);
 
-        words.add(new Word("pai", "әpә", R.drawable.family_father));
-        words.add(new Word("mãe", "әta", R.drawable.family_mother));
-        words.add(new Word("filho", "angsi", R.drawable.family_son));
-        words.add(new Word("filha", "tune", R.drawable.family_daughter));
-        words.add(new Word("irmão mais velho", "taachi", R.drawable.family_older_brother));
-        words.add(new Word("irmão mais novo", "chalitti", R.drawable.family_younger_brother));
-        words.add(new Word("irmã mais velha", "teṭe", R.drawable.family_older_sister));
-        words.add(new Word("irmã mais nova", "kolliti", R.drawable.family_younger_sister));
-        words.add(new Word("avó", "ama", R.drawable.family_grandmother));
-        words.add(new Word("avô", "paapa", R.drawable.family_grandfather));
+        words.add(new Word("pai", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("mãe", "әta", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("filho", "angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("filha", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("irmão mais velho", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new Word("irmão mais novo", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new Word("irmã mais velha", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("irmã mais nova", "kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new Word("avó", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        words.add(new Word("avô", "paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
 
-//        WordAdpter adpter = new WordAdpter(this, words, R.color.category_family);
         WordAdpter adpter = new WordAdpter(this, words, R.color.category_family);
 
         ListView listView = findViewById(R.id.word_list);
         listView.setAdapter(adpter);
+
+
     }
 }
