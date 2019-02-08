7d22ce74eb417a999543198a7a023db9ff3d152f
Paulo <p.henriquerm@hotmail.com>
2019-02-08 14:02:34 -0300

    Passando audios de cores para a lista de palavras

--- app/src/main/java/com/exemplo/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/ColorsActivity.java
@@ -19,14 +19,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<>();
 
-        words.add(new Word("vermelho", "wetetti", R.drawable.color_red));
-        words.add(new Word("verde", "chokokki", R.drawable.color_green));
-        words.add(new Word("marron", "takaakki", R.drawable.color_brown));
-        words.add(new Word("cinza", "topoppi", R.drawable.color_gray));
-        words.add(new Word("preto", "kululli", R.drawable.color_black));
-        words.add(new Word("branco", "kelelli", R.drawable.color_white));
-        words.add(new Word("amarelo empoeirado", "topiisә", R.drawable.color_dusty_yellow));
-        words.add(new Word("amarelo mostarda", "chiwiitә", R.drawable.color_mustard_yellow));
+        words.add(new Word("vermelho", "wetetti", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("verde", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("marron", "takaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("cinza", "topoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("preto", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("branco", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("amarelo empoeirado", "topiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("amarelo mostarda", "chiwiitә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
 
         WordAdpter adpter = new WordAdpter(this, words, R.color.category_colors);
