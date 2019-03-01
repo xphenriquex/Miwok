e2cbef138286eec385ffe34a695873e21dfe27a7
Paulo <p.henriquerm@hotmail.com>
2019-03-01 16:03:08 -0300

    Passando fragmente e titulo dos tabLayout

--- app/src/main/java/com/exemplo/android/miwok/MainActivity.java
+++ app/src/main/java/com/exemplo/android/miwok/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.activity_main);
+
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        adapter.addFragmentTab(new NumbersFragment(), "Números");
+        adapter.addFragmentTab(new FamilyFragment(), "Família");
+        adapter.addFragmentTab(new ColorsFragment(), "Cores");
+        adapter.addFragmentTab(new PhrasesFragment(), "Frases");
 
         ViewPager viewPager = findViewById(R.id.category_pager_view);
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 
